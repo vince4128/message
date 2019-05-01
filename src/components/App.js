29e73fa75ef4afcf0173db71ff223a8a372e463d
varlet vincent <vrlt.vincent@gmail.com>
2019-05-01 18:45:35 +0200

    Routing added
    Routes added using BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import MessageList from './messages/MessageList';
+import MessageShow from './messages/MessageShow';
+import MessageCreate from './messages/MessageCreate';
+
+/**
+ * # Message client
+ * This is a react application whose objective is to display messages coming from a REST API and to propose a form to write them.
+ *
+ * This project has been generated using create-react-app see the documentation here :
+ * https://github.com/facebook/create-react-app/blob/master/README.md
+ *
+ * This component App.js is the root of the application
+ */
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>App</div>
+      <Route path="/" exact component={MessageList} />
+      <Route path="/show/:id" component={MessageShow} />
+      <Route path="/create" component={MessageCreate} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
