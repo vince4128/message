bf16cb0bbdf3827183d37164ba5e15b72aeedafa
varlet vincent <vrlt.vincent@gmail.com>
2019-05-01 22:42:38 +0200

    Dependencies added for handling asynchronous request
    Axios allow us to make requests to the api, and redux-thunk help with the asynchronicity of those requests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
