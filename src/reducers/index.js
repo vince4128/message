c95062865b266ff483a24f633710dbc286c2f0b3
varlet vincent <vrlt.vincent@gmail.com>
2019-05-02 23:18:23 +0200

    Redux Form added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import messageReducer from './messageReducer';
 
 export default combineReducers({
-  messages: messageReducer
+  messages: messageReducer,
+  form: formReducer
 });
