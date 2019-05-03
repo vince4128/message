ff83ceb2b6fc51d39340de79351763b78d927089
varlet vincent <vrlt.vincent@gmail.com>
2019-05-03 11:10:26 +0200

    Render list element
    Separation of concern between the list and the list element renderer

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -2,29 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchMessages } from '../../actions';
 
+import MessageListElement from './MessageListElement';
+
 class MessageList extends Component {
   componentDidMount() {
     this.props.fetchMessages();
   }
 
   renderList(messages) {
-    return messages.map(message => {
-      const { body, title, confidential } = message;
+    return messages.reverse().map(message => {
       return (
-        <div>
-          <h3>{title}</h3>
-          <p>{body}</p>
-          <h4>{confidential ? 'private' : 'public'}</h4>
-          <hr />
-        </div>
+        <li key={message.id}>
+          <MessageListElement message={message} />
+        </li>
       );
     });
-    console.log(messages);
   }
 
   render() {
     if (this.props.messages) {
-      return <div>{this.renderList(this.props.messages)}</div>;
+      return <ul>{this.renderList(this.props.messages)}</ul>;
     }
     return <div>MessageList loading...</div>;
   }
--- src/components/messages/MessageListElement.js
+++ src/components/messages/MessageListElement.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MessageListElement = () => {
-  return <div>MessageListElement</div>;
+const MessageListElement = props => {
+  const { title, body, confidential, id } = props.message;
+
+  return (
+    <React.Fragment>
+      <Link to={`/show/${id}`}>
+        <h3>{title}</h3>
+      </Link>
+      <p>{body}</p>
+      <h4>{confidential ? 'private' : 'public'}</h4>
+      <hr />
+    </React.Fragment>
+  );
 };
 
 export default MessageListElement;
