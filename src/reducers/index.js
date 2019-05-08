2f0d999eab6da40210d233efef25b74fd226ea15
varlet vincent <vrlt.vincent@gmail.com>
2019-05-09 00:56:02 +0200

    Reducers documentation
    Comments added to the reducers related files to document them

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import messageReducer from './messageReducer';
 
+/**
+ * Combine the reducers to form the redux store used in the application.
+ * the store contains messages data and the form data
+ */
+
 export default combineReducers({
   messages: messageReducer,
   form: formReducer
--- src/reducers/messageReducer.js
+++ src/reducers/messageReducer.js
@@ -5,20 +5,26 @@ import {
 } from '../actions/types';
 import _ from 'lodash';
 
+/**
+ * Message reducer, returns a new state for messages based on the actions payload.
+ */
+
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FETCH_MESSAGES:
+      // merge the actual state and the action payload and returns it as a new state
       return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_MESSAGE:
+      //insert or replace an object by using key interpolation syntax in the actual state and returns it as a new state
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_MESSAGE:
       /**
        * For demonstration purpose only
        *
-       * we set the id of the message, so we can merge him into the dataset coming from the fake api
-       * but in production this will be done by the back end api
+       * We set the id of the message, so we can merge him into the dataset coming from the fake api
+       * but in production this will be done by the back end api and the data we will be send by a POST request
        *
        */
       let id = 0;
