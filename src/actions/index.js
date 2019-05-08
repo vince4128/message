93eb4e0a32fef1897c5a96d5d2ef75195525898b
varlet vincent <vrlt.vincent@gmail.com>
2019-05-09 00:58:11 +0200

    Actions documentation
    Comments added to document the redux actions files

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,18 @@ import { FETCH_MESSAGES, FETCH_MESSAGE, CREATE_MESSAGE } from './types';
 import message from '../api/message';
 import history from '../history';
 
+/**
+ * actions for fetch and post data
+ *
+ * *fetchMessages* fetch messages data from the api at the endpoint "/posts"
+ * *fetchMessage* fetch a message from the endpoint "/posts/:id"
+ * *createMessage* add a message to the store
+ *
+ * Fetching data is an asynchronous action to handle that we use redux-thunk.
+ * redux-thunk allow to return a function instead of an object from an action and to manually dispatch the action when the data are present.
+ *
+ */
+
 export const fetchMessages = () => async dispatch => {
   const response = await message.get('/posts');
   dispatch({ type: FETCH_MESSAGES, payload: response.data });
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,7 @@
+/**
+ * export the different types of actions, using variables instead of String make the application easier to debug
+ */
+
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
 export const FETCH_MESSAGE = 'FETCH_MESSAGE';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
